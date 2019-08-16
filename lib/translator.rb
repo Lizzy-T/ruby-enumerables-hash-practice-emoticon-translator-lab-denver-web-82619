7cc4f20a4eef0fa7c1aa620c452eb0d711a153be
Lizzy Tong <lizzy.tong33@gmail.com>
2019-08-16 21:30:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,12 @@
 
 emoticons = {}
-require "yaml"
-emoticons = YAML.load_file('/home/RiverCreature/ruby-enumerables-hash-practice-emoticon-translator-lab-denver-web-82619/lib/emoticons.yml')
-p emoticons
-exit
+
 
 def load_library
-
+  require "yaml"
+  emoticons = YAML.load_file('/home/RiverCreature/ruby-enumerables-hash-practice-emoticon-translator-lab-denver-web-82619/lib/emoticons.yml')
+  p emoticons
+  exit
 
 end
 
