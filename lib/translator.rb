fe4cd9a8641db7a0f4919f61513e375a5b78360f
Lizzy Tong <lizzy.tong33@gmail.com>
2019-08-16 22:12:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,11 +4,13 @@ file_path = "/home/RiverCreature/ruby-enumerables-hash-practice-emoticon-transla
 organized_emojis = {:get_meaning => {},
                     :get_emoticon => {}}
 def load_library(file_path)
+  p "-------------------- start ----------------------"
   require "yaml"
   emoticon_raw = YAML.load_file(file_path)
   emoticon_raw.each_pair do |meaning, value|
     english = value[0]
     japan = value[1]
+    p "#{meaning} : #{english}  #{japan}"
     value.each do |emoji|
       if emoji == japan
         organized_emojis[:get_meaning][japan] = [meaning]
