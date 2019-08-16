48d4820567a063d24601a47dc09e0127fac983c3
Lizzy Tong <lizzy.tong33@gmail.com>
2019-08-16 22:02:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(file_path)
   emoticons.each_pair do |meaning, value|
     english = value[0]
     japan = value[1]
-    value.each do
+    value.each do |emoji|
 
       binding.pry
       exit
